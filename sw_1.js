d1573b6be164bd38b890a55cbae76bcc675a0daf
mrjyral <mrjyral@gmail.com>
2026-07-24 18:28:24 +0200

    feat: V4.2 Ziele Fix

---
+++ sw_1.js
@@ -0,0 +1,15 @@
+
+const CACHE = 'mission2027-v4-fixed';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request, {cache: 'no-store'}).then(r => {
+      return r;
+    }).catch(()=>caches.match(e.request))
+  );
+});
