5cf87239b89ee84a2afe2a67bbb7639a4640c6b1
mrjyral <mrjyral@gmail.com>
2026-07-24 18:47:44 +0200

    feat: V5 KAI 4-20 Kontakte

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 
-const CACHE = 'mission2027-v3-final';
-const ASSETS = ['/','/index.html','/manifest.json','/icon-192.png','/icon-512.png'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => { const clone=res.clone(); caches.open(CACHE).then(c=>c.put(e.request, clone)); return res; }).catch(()=>caches.match('/index.html')))); });
+const CACHE='mission2027-v5-kai';
+self.addEventListener('install', e=>self.skipWaiting());
+self.addEventListener('activate', e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(c=>caches.delete(c)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch', e=>{e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))});
